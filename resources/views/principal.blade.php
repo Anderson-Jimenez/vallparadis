c2b7d15edf12936733159247708abf33bd38c6db
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-08 11:57:54 +0200

    versio senzilla login tailwind

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -14,15 +14,17 @@
 <body class="flex justify-center items-center bg-forms h-screen">
     <div class="flex bg-white-transparent w60 h-35vw text-center items-center rounded-2xl">
         <section class="w50">
+            <img src="{{ asset('img/logo.png') }}" alt="logo_vallparadis">
             <h1>Benvinguts a la fundació VallParadís!</h1>
         </section>
-        <section class="w40">
+        <section class="w40 text-center">
+            <img src="{{ asset('img/login/Login_icon.png') }}" alt="icone_login" class="h-10vw">
             <h3>Login</h3>
-            <form action="index.php" method="POST" class="row">
+            <form action="index.php" method="POST" class="flex flex-wrap justify-center">
                 <label for="name" class="w80">Nom d'usuari</label>
                 <input type="name">
-                <label for="username" class="w80">Contrasenya</label>
-                <input type="password" name="passwd" class="w80">
+                <label for="username">Contrasenya</label>
+                <input type="password" name="passwd">
                 <a href="#">Has oblidat la contrasenya?</a>
                 <input type="submit" value="Iniciar sesion">
             </form>
