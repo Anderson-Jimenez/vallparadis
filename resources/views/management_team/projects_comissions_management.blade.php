5be5c6186f3d75ffc66f4102026a06c71181f410
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-12 17:04:06 +0200

    enllaços a tailwind a totes les vistes

--- resources/views/management_team/projects_comissions_management.blade.php
+++ resources/views/management_team/projects_comissions_management.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gestió Projectes/Comissions</title>
+    @vite("resources/css/app.css")
+
 </head>
 <body>
     <h1>Gestió Projectes i Comissions</h1>
