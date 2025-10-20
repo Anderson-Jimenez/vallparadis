de1507eacbff388a50882c98d5e20f733beea16c
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-20 10:09:15 +0200

    Disseny amb Tailwind center_management

--- resources/views/management_team/centers_management.blade.php
+++ resources/views/management_team/centers_management.blade.php
@@ -8,35 +8,39 @@
     @vite("resources/css/app.css")
 
 </head>
-<body class="bg-[#2D3E50]">
+<body class="min-h-screen flex flex-col bg-[#2D3E50]">
     @include('partials.icons')     
     @auth
         @include('components.navbar')
         @yield('contingut')
-            <main class="w-screen flex justify-center flex-wrap h-auto">
-                <h1 class="text-white text-2xl w-screen text-center p-10">Gestió Centre</h1>
-                <table class="table-base table-wrapper w-[80vw]">
+            <main class="flex-grow flex flex-col items-center w-full py-10">
+
+                <h1 class="text-white text-2xl w-10/12 text-center p-10 border-b-6 border-[#ff7300]">Gestió Centre</h1>
+                
+                <table class=" border-solid w-[80vw] m-15">
                     <tr class="table-row">
-                        <th class="table-cell">Nom</th>
-                        <th class="table-cell">Ubicació</th>
-                        <th class="table-cell">Teléfon</th>
-                        <th class="table-cell">Email</th>
+                        <th class="p-4 text-sm txt-orange hover:bg-[#b4b4b459] transition-all duration-300">Nom</th>
+                        <th class="p-4 text-sm txt-orange hover:bg-[#b4b4b459] transition-all duration-300">Ubicació</th>
+                        <th class="p-4 text-sm txt-orange hover:bg-[#b4b4b459] transition-all duration-300">Teléfon</th>
+                        <th class="p-4 text-sm txt-orange hover:bg-[#b4b4b459] transition-all duration-300">Email</th>
                     </tr>
 
                     @foreach ($centers as $center)
                         <tr class="table-row">
-                            <td class="table-cell">{{ $center->center_name }}</td>
-                            <td class="table-cell">{{ $center->location }}</td>
-                            <td class="table-cell">{{ $center->phone_number }}</td>
-                            <td class="table-cell">{{ $center->email_address }}</td>
-                            <td class="table-cell"><a href="">Modificar</a></td>
-                            <td class="table-cell"><a href="">Eliminar</a></td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300">{{ $center->center_name }}</td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300">{{ $center->location }}</td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300">{{ $center->phone_number }}</td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300">{{ $center->email_address }}</td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300"><a href="">Modificar</a></td>
+                            <td class="p-4 text-sm hover:bg-[#b4b4b459] hover:text-[#ff7300] transition-all duration-300"><a href="">Eliminar</a></td>
                         </tr>
                     @endforeach
                 
                 </table>
 
-                <a href="{{route('center.create')}}" class="text-center text-white p-10">Afegir Centre</a>
+                <a class="text-lg text-white bg-[#ff7300] hover:bg-white hover:text-[#ff7300] transition-all duration-300 rounded-full p-8">
+                Afegir Centre
+                </a>
             </main>
             
         @include('components.footer')
