aefe024fe01cf151600d53c352e7178fc82095bc
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-21 10:02:23 +0200

    ajustament pagina principal i login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="w-screen flex items-center justify-between p-4 bg-white border-b-15 border-[#ff7300]">
-    <img src="{{ asset('img/logo.png') }}" alt="logo_vallparadis" class="w-[15vw]">
+    <img src="{{ asset('img/logo.png') }}" alt="logo_vallparadis" class="w-[13vw]">
     <div>
             <ul class="flex items-center mr-13">
-            <li class="m-3 txt-orange text-2xl">
+            <li class="m-3 txt-orange text-lg">
                     "barra de busqueda"
             </li>
-            <li class="m-3 txt-orange text-2xl">Hola <strong>{{ Auth::user()->name }}</strong>👋</li>
+            <li class="m-3 txt-orange text-lg">Hola <strong>{{ Auth::user()->name }}</strong>👋</li>
         </ul>
     </div>
 </nav>
--- resources/views/management_team/principal.blade.php
+++ resources/views/management_team/principal.blade.php
@@ -14,16 +14,16 @@
 
         @yield('contingut')
             <main class="w-screen flex">
-                <aside class="w-1/4 bg-[#2D3E50] p-8 h-[50vw] flex flex-col justify-between">
+                <aside class="bg-[#2D3E50] p-8 h-[45vw] flex flex-col justify-between w-1/5">
                     <ul>
                         <li class="group my-4 rounded-lg transition-all duration-300">
-                            <a href="{{ route('center.index') }}" class="flex items-center gap-3 p-4 rounded-lg bg-[#2D3E50] group-hover:bg-[#ff7300] transition-all duration-300 w-max">
+                            <a href="{{ route('center.index') }}" class="flex items-center gap-3 p-4 rounded-lg bg-[#2D3E50] group-hover:bg-[#ff7300] transition-all duration-300 ">
                             
-                                <svg class="w-10 h-10 text-white">
+                                <svg class="w-8 h-8 text-white">
                                     <use xlink:href="#professional_icon"></use>
                                 </svg>
 
-                                <span class="text-white text-2xl">
+                                <span class="text-white text-lg">
                                     Gestió Centre
                                 </span>
                             </a>
@@ -31,11 +31,11 @@
                         <li class="group my-4 rounded-lg transition-all duration-300">
                             <a href="{{ route('professional.index') }}" class="flex items-center gap-3 p-4 rounded-lg bg-[#2D3E50] group-hover:bg-[#ff7300] transition-all duration-300">
                             
-                                <svg class="w-10 h-10 text-white">
+                                <svg class="w-8 h-8 text-white">
                                     <use xlink:href="#center_icon"></use>
                                 </svg>
 
-                                <span class="text-white text-2xl">
+                                <span class="text-white text-lg">
                                     Gestió Professionals
                                 </span>
                             </a>
@@ -43,28 +43,29 @@
                         <li class="group my-4 rounded-lg transition-all duration-300">
                             <a href="{{ route('project_comission.index') }}" class="flex items-center gap-3 p-4 rounded-lg bg-[#2D3E50] group-hover:bg-[#ff7300] transition-all duration-300">
                             
-                                <svg class="w-12 h-12 text-white">
+                                <svg class="w-10 h-10 text-white">
                                     <use xlink:href="#project_icon"></use>
                                 </svg>
 
-                                <span class="text-white text-2xl">
+                                <span class="text-white text-lg">
                                     Gestió Projectes i comissions
                                 </span>
                             </a>
                         </li>
                     </ul>   
-                    <a href="{{ route('logout') }}" class="group flex justify-center items-center top-20 w-[15vw] h-[4vw] bg-white text-[#ff7300] px-6 py-4 rounded-full text-2xl font-semibold
+                    <a href="{{ route('logout') }}" class="group flex justify-center items-center top-20 w-[15vw] h-[3vw] bg-white text-[#ff7300] px-3 py-1 rounded-full text-lg font-semibold
                     transition-all duration-300 hover:bg-[#ff7300] hover:text-white">
-                        Cerrar sessió
+                        Tancar sessió
                     </a>
                 </aside>
                 
                 <!--S'haria de posar un controllador o quelcom similar-->
-                <section class="w-3/4">
+                <section class=" w-4/5">
                     <div class="flex w-full h-max">
-                        <div class="w-2/6 h-[20vh] bg-[#ff7300] m-7 rounded-3xl"></div>
-                        <div class="w-2/6 h-[20vh] bg-[#ff7300] m-7 rounded-3xl"></div>
-                        <div class="w-2/6 h-[20vh] bg-[#ff7300] m-7 rounded-3xl"></div>
+                        <div class="w-1/4 h-[20vh] bg-[#949494] m-4 rounded-3xl"></div>
+                        <div class="w-1/4 h-[20vh] bg-[#949494] m-4 rounded-3xl"></div>
+                        <div class="w-1/4 h-[20vh] bg-[#949494] m-4 rounded-3xl"></div>
+                        <div class="w-1/4 h-[20vh] bg-[#949494] m-4 rounded-3xl"></div>
                     </div>
 
                 </section>
