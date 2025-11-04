f3c7a5d17f49049698f1e29ccd123ebdeb63a930
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-11-04 10:43:33 +0100

    modificacio migracions

--- database/migrations/2025_09_29_214841_create_evaluations_table.php
+++ database/migrations/2025_09_29_214841_create_evaluations_table.php
@@ -19,7 +19,9 @@ return new class extends Migration
 
             $table->foreign('professional_id')->references('id')->on('professionals')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('professional_assessed_id')->references('id')->on('professionals')->onUpdate('cascade')->onDelete('cascade');
-
+            $table->string('name',255);
+            $table->string('filesize',255);
+            $table->string('path',255);
             $table->timestamps();
         });
     }
