a199a792f57d176974c0b711d762aaa1381ecd90
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2026-01-19 16:59:30 +0100

    ajustes

--- resources/views/services/general/followup.blade.php
+++ resources/views/services/general/followup.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gestió Professionals</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/general_service_followup.js'])
 </head>
 
 <body class="min-h-screen flex flex-col bg-[#E9EDF2]">
